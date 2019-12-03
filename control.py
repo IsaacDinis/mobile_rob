3f7eb25e488886504a1a8c2a613f19a7058d4db6
Loïc <lferrot@hotmail.fr>
2019-12-03 17:16:31 +0100

    Ajouté une fonction de turn+move, recalibré les valeurs pour la map en A0

--- control.py
+++ control.py
@@ -4,8 +4,12 @@ import numpy as np
 import utils as ut
 from Thymio_custom import Thymio
 
-MOV_ANGLE = 1
-MOV_STRAIGHT = 2
+ANGLE_ONLY = 1
+LINE_ONLY = 2
+ANGLE_LINE = 3
+LINE_ANGLE = 4
+
+MAX_DIST = 42
 
 
 def turn_angle(thymio, angle):
@@ -14,20 +18,36 @@ def turn_angle(thymio, angle):
     if thym_angle < 0:
         thym_angle += 2**16
     thymio.set_var("angle_set", thym_angle)
-    thymio.set_var("movement_mode", MOV_ANGLE)
+    thymio.set_var("movement_mode", ANGLE_ONLY)
 
 
 def move_distance(thymio, dist):
-    """ Move the Thymio for a distance between +-20cm"""
-    thym_dist = round(dist*(2**15-1)/20)
+    """ Move the Thymio for a distance between +-42cm"""
+    thym_dist = round(dist*(2**15-1)/MAX_DIST)
     if thym_dist < 0:
         thym_dist += 2**16
     thymio.set_var("dist_set", thym_dist)
-    thymio.set_var("movement_mode", MOV_STRAIGHT)
+    thymio.set_var("movement_mode", LINE_ONLY)
+
+
+def turn_angle_move_distance(thymio, angle, dist, angle_first=True):
+    """ Turn the Thymio for an angle between +- 2pi"""
+    thym_angle = round(angle*(2**15-1)/(2*np.pi))
+    if thym_angle < 0:
+        thym_angle += 2**16
+    thym_dist = round(dist*(2**15-1)/MAX_DIST)
+    if thym_dist < 0:
+        thym_dist += 2**16
+    thymio.set_var("angle_set", thym_angle)
+    thymio.set_var("dist_set", thym_dist)
+    if angle_first:
+        mode = ANGLE_LINE
+    else:
+        mode = LINE_ANGLE
+    thymio.set_var("movement_mode", mode)
 
 
 if __name__ == "__main__":
-# %%
     import time
     thymio = Thymio.serial(port="COM14", refreshing_rate=0.1)
 
