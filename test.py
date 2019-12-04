2e647b80f214bdbd1aec4cbff90d2581f3141596
Isaac Dinis <isaac.dinis@epfl.ch>
2019-12-04 11:36:29 +0100

    added flip image

--- test.py
+++ test.py
@@ -2,6 +2,15 @@ from vision import *
 import cv2
 
 img = cv2.imread("map_test.PNG")
+img = cv2.flip(img, 0)
+# cap = cv2.VideoCapture(1 , cv2.CAP_DSHOW)
+# while True:
+#     _, img = cap.read()
+#     cv2.imshow("Display window", img)
+#     k = cv2.waitKey(5) & 0xFF
+#     if k == 27:
+#         break
+
 cv2.imshow("Display window", img)
 warped = map_projection(img)
 scale_percent = 150  # percent of original size
@@ -19,3 +28,4 @@ draw_obstacles(resized, obstacles)
 cv2.imshow("projected", resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+# cap.release()
