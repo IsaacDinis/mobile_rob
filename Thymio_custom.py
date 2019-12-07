38e40ca952cdc01d66ed3685b7c296d58771e251
Isaac Dinis <isaac.dinis@epfl.ch>
2019-12-07 17:12:37 +0100

    improved local navigation

--- Thymio_custom.py
+++ Thymio_custom.py
@@ -6,6 +6,7 @@
 import threading
 import numpy as np
 import time
+from local_avoidance import *
 
 
 class Message:
@@ -349,6 +350,7 @@ class Thymio:
         self.refreshing_timeout = None
         self.refreshing_trigger = threading.Event()  # initially wait() blocks
         self.reset_odom = False
+        self.nav_flag = "global"
 
         def do_refresh():
             while not self.terminating:
@@ -357,6 +359,13 @@ class Thymio:
                 self.get_variables()
 
                 self.increment_odometry()
+                direction = check_obstacle(self)
+
+                if direction != "none":
+                    self.nav_flag = "local"
+
+                if self.nav_flag == "local":
+                    local_avoidance(self, direction)
                 # if self.reset_odom:
                 #     # ## debug
                 #     # table = self["event.args"]
--- local_avoidance.py
+++ local_avoidance.py
@@ -4,35 +4,56 @@ import numpy as np
 from time import sleep
 
 
-def local_avoidance(thymio):
-
+def check_obstacle(self):
     threshold = 2000
     if thymio["prox.horizontal"][0] > threshold:
-        new_val = thymio["prox.horizontal"][2]
-        old_val = 0
-        while new_val >= old_val:
-            turn_angle(thymio, 1)  # turn left with angle = 1 rad
-            while thymio["event.args"][12]:
-                pass
-            old_val = new_val
-            new_val = thymio["prox.horizontal"][2]
-        # thymio is now facing the obstacle
-        turn_angle(thymio, -np.pi / 2)
+        return "right"
+    elif thymio["prox.horizontal"][4] > threshold:
+        return "left"
+    else:
+        return "none"
+
+
+def local_avoidance(thymio, direction):
+
+    new_val = thymio["prox.horizontal"][2]
+    old_val = 0
+    while new_val >= old_val:
+        if direction == "left":
+            turn_angle(thymio, 0.1)  # turn left with angle = 1 rad
+        elif direction == "right":
+            turn_angle(thymio, -0.1)  # turn left with angle = 1 rad
         while thymio["event.args"][12]:
             pass
+        old_val = new_val
+        new_val = thymio["prox.horizontal"][2]
 
-        while True:
-            move_distance(thymio, 5)
-            while thymio["event.args"][12]:
-                pass
+    # thymio is now facing the obstacle
+    if direction == "left":
+        turn_angle(thymio, -np.pi / 2)
+    elif direction == "right":
+        turn_angle(thymio, np.pi / 2)
+    while thymio["event.args"][12]:
+        pass
+
+    while True:
+        move_distance(thymio, 10)
+        while thymio["event.args"][12]:
+            pass
+        if direction == "left":
             turn_angle(thymio, np.pi/2)
-            while thymio["event.args"][12]:
-                pass
-            if thymio["prox.horizontal"][2] == 0:
-                break
+        elif direction == "right":
             turn_angle(thymio, -np.pi/2)
-            while thymio["event.args"][12]:
-                pass
+        while thymio["event.args"][12]:
+            pass
+        if thymio["prox.horizontal"][2] == 0:
+            break
+        if direction == "left":
+            turn_angle(thymio, -np.pi/2)
+        elif direction == "right":
+            turn_angle(thymio, np.pi/2)
+        while thymio["event.args"][12]:
+            pass
 
     return
 
